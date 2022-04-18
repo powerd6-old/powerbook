e20376187e7c36c9eefbe0ff6cbea093c967d775
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2022-04-18 19:33:11 +0200

    feat: add chapter with content

--- lib/models/content.dart
+++ lib/models/content.dart
@@ -1,6 +1,9 @@
 export 'content/markdown_content.dart' show MarkdownContent;
 export 'content/template_content.dart' show TemplateContent;
 
+import 'package:powerbook/models/content/markdown_content.dart';
+import 'package:powerbook/models/content/template_content.dart';
+
 abstract class Content {
   final String type;
 
@@ -12,5 +15,20 @@ abstract class Content {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['type'] = type;
     return data;
+  }
+}
+
+class ContentMapper {
+  static Content map(Map<String, dynamic> dynamicContent) {
+    switch (dynamicContent['type']) {
+      case 'markdown':
+        return MarkdownContent.fromJson(dynamicContent);
+      case 'template':
+        return TemplateContent.fromJson(dynamicContent);
+      default:
+        throw UnimplementedError(
+            "ContentMapper is not able to map a content of type: " +
+                dynamicContent['type']);
+    }
   }
 }
