c2166ec849cd4ef244d7233e84bccf981838bb7f
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2022-04-24 22:36:12 +0200

    feat: remove unnused json code from models

--- lib/models/author.dart
+++ lib/models/author.dart
@@ -5,21 +5,6 @@ class Author {
 
   Author({required this.name, required this.email, this.twitter});
 
-  Author.fromJson(Map<String, dynamic> json)
-      : name = json['name'],
-        email = json['email'],
-        twitter = json['twitter'];
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['name'] = name;
-    data['email'] = email;
-    if (twitter != null) {
-      data['twitter'] = twitter;
-    }
-    return data;
-  }
-
   @override
   bool operator ==(Object other) {
     return other is Author && email == other.email;
